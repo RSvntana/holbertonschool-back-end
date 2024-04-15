abf52445b4508e3e157a05eca319929c65ed8828
Robert <123roabel@gmail.com>
2024-04-15 19:50:21 -0400

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -27,15 +27,10 @@ def export_todo_progress(employee_id):
     employee_name = get_employee_name(employee_id)
     filename = f"{employee_id}.csv"
 
-    with open(filename, mode='w', newline='') as csvfile:
+    with open(filename, mode="w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            row = [
-                employee_id,
-                employee_name,
-                task.get("completed"),
-                task.get("title")
-            ]
+            row = [employee_id, employee_name, task.get("completed"), task.get("title")]
             csv_writer.writerow(row)
 
 
